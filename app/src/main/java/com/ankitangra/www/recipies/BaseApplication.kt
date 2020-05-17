44939b3ddae91815bcbfb15b795c25c2f8198c18
Angra, Ankit <ankit.angra@bellmedia.ca>
2020-05-17 17:17:26 -0400

    Added Dagger and Created App Component

--- app/src/main/java/com/ankitangra/www/recipies/BaseApplication.kt
+++ app/src/main/java/com/ankitangra/www/recipies/BaseApplication.kt
@@ -1,6 +1,22 @@
 package com.ankitangra.www.recipies
 
 import android.app.Application
+import com.ankitangra.www.recipies.di.AppComponent
+import com.ankitangra.www.recipies.di.DaggerAppComponent
 
 class BaseApplication : Application() {
+
+    lateinit var appComponent : AppComponent
+
+    override fun onCreate() {
+        super.onCreate()
+        initAppComponent()
+    }
+
+    private fun initAppComponent () {
+        appComponent = DaggerAppComponent.builder()
+            .application(this)
+            .build()
+    }
+
 }
--- app/src/main/java/com/ankitangra/www/recipies/di/AppComponent.kt
+++ app/src/main/java/com/ankitangra/www/recipies/di/AppComponent.kt
@@ -1,4 +1,18 @@
 package com.ankitangra.www.recipies.di
 
+import android.app.Application
+import dagger.BindsInstance
+import dagger.Component
+
+@Component
 interface AppComponent {
+
+    @Component.Builder
+    interface Builder {
+
+        @BindsInstance
+        fun application (application: Application) : Builder
+
+        fun build() : AppComponent
+    }
 }
